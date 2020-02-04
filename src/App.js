8061ba568e5995f114a3a7341e663457b32be9c0
Mamun746 <abdullahalmamun914@gmail.com>
2020-02-04 15:09:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state={
+    recipes:[],
+    searchRecipe:''
+  }
+  componentDidMount(){
+    
+  }
+
+  handleOnchange=(e)=>{
+    this.setState({
+      searchRecipe:e.target.value
+    })
+  }
+
+  submitRecipe=(e)=>{
+    e.preventDefault()
+
+    axios.get(`http://recipesapi.herokuapp.com/api/search?q=${this.state.searchRecipe}`)
+    .then((res)=>{
+      const data=res.data.recipes
+      this.setState({
+          recipes:data
+      })
+      
+    })
+    
+
+  }
+
+
+
+
+
+  // componentDidMount() {
+  //   const json = localStorage.getItem("recipes");
+  //   const recipes = JSON.parse(json);
+  //   this.setState({
+  //     recipes
+  //   });
+  // }
+  // componentDidUpdate() {
+  //   const recipes = JSON.stringify(this.state.recipes);
+  //   localStorage.setItem("recipes", recipes);
+  // }
+  render() {
+    return (
+      <div>
+      <form onSubmit={this.submitRecipe}>
+      <input type="text" value={this.state.searchRecipe} onChange={this.handleOnchange}/>
+      
+      </form>
+      {
+        this.state.recipes.map((recipe)=>{
+          return <img height='300px' width="300px" src={recipe.image_url} alt='mamun'/>
+        })
+      }
+       <h1>Hello</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+
